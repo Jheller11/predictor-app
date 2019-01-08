9ba9d93e8a3ad385515605a7d83ce111636db55c
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-01-08 18:29:27 -0500

    config games index and single week links

--- controllers/games.js
+++ controllers/games.js
@@ -7,11 +7,16 @@ router.get('/:matchday', (req, res) => {
   let currentWeek = req.params.matchday
   Game.find({ matchday: currentWeek })
     .then(games => {
-      res.render('games/index', { games: games })
+      res.render('games/matchday', { games: games })
     })
     .catch(err => {
       res.render('error', { error: err })
     })
+})
+
+// get weeks list
+router.get('/', (req, res) => {
+  res.render('games/index')
 })
 
 module.exports = router
